fe39afc8afe4bfb5063d6f4e22617d0f336dd981
priyaankchhadwa <priyaank29@gmail.com>
2020-10-23 22:00:51 -0400

    completed week07 instructions

---
+++ server.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+    next();
+});
+
+const jwt = require('jsonwebtoken');
+const exjwt = require('express-jwt');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 3000;
+
+const secretKey = 'My super secret key';
+const jwtMW = exjwt({
+    secret: secretKey,
+    algorithms: ['HS256']
+});
+
+let users = [
+    {
+        id: 1,
+        username: 'priyaank',
+        password: '123'
+    },
+    {
+        id: 2,
+        username: 'chhadwa',
+        password: '456'
+    },
+];
+
+
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+    for (let user of users) {
+        if (username == user.username && password == user.password) {
+            let token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '3m' });
+            res.json({
+                success: true,
+                err: null,
+                token
+            });
+            break;
+        }
+        else {
+            res.status(401).json({
+                success: false,
+                token: null,
+                err: 'Username or password is incorrect'
+            });
+        }
+
+    }
+});
+
+app.get('/api/dashboard', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'Secret content that only logged in people can see'
+    });
+});
+
+
+app.get('/api/settings', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'The settings are here!'
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({
+            success: false,
+            officialError: err,
+            err: 'Username or password is not correct'
+        });
+    }
+    else {
+        next(err);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`serving on port ${PORT}`);
+});
